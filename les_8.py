a12fa2127e84baeb786c8f92015747d9fa4e09e9
Pol13 <110257344+Polinka1304@users.noreply.github.com>
2023-09-14 20:45:09 +0300

    Update les_8.py

--- les_8.py
+++ les_8.py
@@ -1,6 +1,8 @@
 from aiogram import Bot, types, Dispatcher
 from aiogram.filters import Command
+from aiogram import F
 import asyncio
+import json
 from aiogram.types.web_app_info import WebAppInfo
 
 
@@ -10,11 +12,17 @@ dp = Dispatcher()
 @dp.message(Command('start'))
 async def start(message: types.Message):
     bt = [
-        [types.KeyboardButton(text = 'Открыть веб страницу',web_app = WebAppInfo(url = 'https://polinka1304-psychic-space-journey-9r567g7755gfp646-8080.app.github.dev/index.html'))]
+        [types.KeyboardButton(text = 'Открыть веб страницу',web_app = WebAppInfo(url = 'https://polinka1304.github.io/Polina/'))]
     ]
     markup = types.ReplyKeyboardMarkup(keyboard = bt)
     await message.answer('Привет!!!', reply_markup = markup)
 
+
+@dp.message(F.web_app_data)
+async def web_app(message: types.Message):
+    #from our app
+    res = json.loads(message.web_app_data.data)
+    await message.answer(f"Name: {res['name']}. Email: {res['email']}. Phone: {res['phone']}")
 
 
 
